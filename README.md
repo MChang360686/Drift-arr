32de5f9fe11d22073266db0a996871122c5aaff7
MChang360686 <readingcyclist@gmail.com>
2023-09-02 00:08:28 -0400

    dictionaries finished

--- README.md
+++ README.md
@@ -6,21 +6,25 @@ Basic Premise - Keep track of Ultimate Parents using an Associative Array (Dict)
 TODO
 1) Write Python version
 2) Make a flowchart graphic and add comments
-3) Write a C version
-4) Write script to compare results for Python and C
-5) Write a Java version
+3) Make a MySQL DB and compare
+4) Write a C version
+5) Write script to compare results for Python and C
+6) Write a Java version
 
-Notes
-- What if we have multiples of an account in accounts.csv?
-- How do we tell which account is the ultimate parent, parent, or child?
-- How do we sum up the money?
-- What if we do this multiple times?
-    - Search using Pandas?
 
 Assumptions
-- Each account is "unique" for the provided dataset
-    - How do we deal with duplicates though?
-        - Try catch finally?
-        - Monads?
-- Companies only pay when the subscription ends
-    - Date is type str when we pull it from csv
+- Each account is "unique" for the provided dataset in accounts.csv
+    - May not be the case for other datasets
+- Companies are considered when the subscription is active
+    - We're counting the money we are getting from them
+    while the subscription is active as revenue
+- for hierarchy_arr, I assume that it should be filled with
+how much money the hierarchy INCLUDING the given account NOT
+how much money the hierarchy as a WHOLE has made
+
+Unit Tests
+- Date - what if today is the deadline?
+- Money - Are we getting the full amount?
+
+Questions
+- is if(statement): or if(statement == True): better? 
--- arr.py
+++ arr.py
@@ -1,46 +1,46 @@
 import pandas as pd
 from datetime import datetime
 from dotenv import load_dotenv
+import time
+import argparse
 
 
 # TODO figure out how to get the Parents IDs and figure out who the Ultimate Parent is
+# TODO Make Pseudocode -> Make code for 2 new functions
 # TODO how to differentiate from dmy instead of american mdy
-# TODO Make Flowchart
-# TODO Decide if I should make a 4th dictionary of ultimate parents to double check?
+# TODO unit testing
 
 
 # lambda functions 
 add = lambda a, b: a + b
 sub = lambda a, b: a - b
 total = lambda quantity, price, discount: quantity * price * (1 - discount)
-existsInDict = lambda key, dict: print(f"Duplicate Found: {key}") if key in dict else ''
+duplicateAccount = lambda key, dict: print(f"Duplicate Found: {key}") if key in dict else ''
+existsDict = lambda key, dict: True if key in dict else False
 
-# Compares given string date with today's date
-# to check if a subscription is still valid
-def subscrpOver(endDate):
+"""
+Compares given string date with today's date
+to check if a subscription is still valid
+"""
+def countRevenue(endDate):
     today = datetime.now().date()
     compareDate = datetime.strptime(endDate, '%Y-%m-%d').date()
-    print(today, compareDate)
-    if(today < compareDate):
-        # subscription end date is in the future
-        # subscription is NOT over
-        return False
-    else:
-        # subscription end date is past or today
-        # subscription IS over
-        # parent/child owes money
+    if(today <= compareDate):
+        # End date is today or in the future subscription is NOT over
         return True
+    elif(today > compareDate):
+        # End date is in the past, subscription IS over
+        return False
 
 
 # Return dict with accountName : parentAccount
-def sort(accts):
+def sortAccounts(accts):
     acctAndPrnt = {}
     df = pd.read_csv(accts)
 
-    # First add everything to a dict so access time is O(1)
     for index, row in df.iterrows():
-        # Check if already exists in dict
-        existsInDict(row['id'], acctAndPrnt)
+        # Check if account already exists in dict
+        duplicateAccount(row['id'], acctAndPrnt)
 
         # Otherwise, just add items to dict
         acctID = row['id']
@@ -51,36 +51,107 @@ def sort(accts):
     return acctAndPrnt
 
 
-# Return dict with accountName: subscriptionID
-def acctSubscriptions(subscritems):
+# Return dict with subscriptionID: accountID
+def acctSubscriptions(subscriptions):
     acctAndSubscription = {}
-    df = pd.read_csv()
-    # If quantity = 0 OR discount = 1 we can skip
-    # They either bought nothing OR it was free
-    #if (quant == 0 or disc == 1):
-    #    pass
-    # Check if 
-    #elif(quant > 0 and disc < 1.0):
-    #    return total(quant, prce, disc)
+    df2 = pd.read_csv(subscriptions)
+
+    for index, row in df2.iterrows():
+        # Check if account already exists in dict
+        duplicateAccount(row['id'], acctAndSubscription)
+
+        # Otherwise, just add items to dict
+        subID = row['id']
+        acctID = row['account_id']
+        acctAndSubscription[subID] = acctID
 
     print('Finished dict 2')
     return acctAndSubscription
 
 
 # Return dict with subscriptionID: moneyOwed
-def moneyOwed():
+def moneyOwed(subscriptionItems):
     subscriptionAndOwed = {}
+
+    df3 = pd.read_csv(subscriptionItems)
+
+    for index, row in df3.iterrows():
+        #id = row['id']
+        subID = row['subscription_id']
+        quant = row['quantity']
+        prce = row['list_price']
+        disc = row['discount']
+        endDate = row['end_date']
+        subtotal = 0
+
+        if (quant == 0 or disc == 1 or countRevenue(endDate) == False):
+            """
+            If quantity = 0 OR discount = 1 OR subscription isn't over we can skip
+            They either bought nothing OR it was free
+            OR their subscription is not active
+            """
+            subtotal = 0
+        elif(quant > 0 and disc < 1.0):
+            # 
+            subtotal = total(quant, prce, disc)
+        else:
+            # Check negative quant and negative disc?
+            print("3rd case in moneyOwed, Possible Error in Dataset")
+
+        """
+        Duplicates exist because some subscriptions get multiple items 
+        Therefore, there needs to be a way to append money
+        """
+
+        if(existsDict(subID, subscriptionAndOwed) == True):
+            # key/subscription exists in dict
+            # We don't want to replace, we want to append
+            subscriptionAndOwed[subID] += subtotal
+        elif(existsDict(subID, subscriptionAndOwed) == False):
+            # key/subscription does NOT exist in dict
+            # Create a new one
+            subscriptionAndOwed[subID] = subtotal
+
     print('Finished Dict 3')
     return subscriptionAndOwed
 
 
+def findUltParent():
+    pass
+
+def getHierarchyArr():
+    pass
+
+
+"""
+Make the 3 dictionaries and fill out
+ultimate_parent, arr, hierarchy_arr 
+using the helper methods
+"""
+def fillColumns(fp, fp2, fp3):
+    dict1 = sortAccounts(fp)
+    dict2 = acctSubscriptions(fp2)
+    dict3 = moneyOwed(fp3)
+
+    df4 = pd.read_csv(fp)
+
+    for index, row in df4.iterows():
+        pass
+    # Finish by writing dataframe to csv
+    df4.to_csv('solutions.csv', index=False)
+
+
 if __name__ == '__main__':
-    filepath = "accounts.csv"
+    startTime = time.time()
+    
+
+    fp = "accounts.csv"
     fp2 = "subscriptions.csv"
     fp3 = "subscription_items.csv"
-    #sort(filepath, fp2, fp3)
+
+    fillColumns(fp, fp2, fp3)
     
-    df = pd.read_csv("subscriptions.csv")
-    line = df.iloc[1]
-    end = line['end_date']
-    print(subscrpOver(end))
+
+    endTime = time.time()
+    elapsedTime = endTime - startTime
+    print(elapsedTime)
